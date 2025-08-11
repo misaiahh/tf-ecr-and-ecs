7183ca903bc61d88d82e4cd62939d6e8adce04a8
Micky Hallabrin <misaiahh@proton.me>
2025-08-11 17:46:07 -0400

    added ecr url as output

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "ecr_repository_name" {
   description = "The name of the ECR repository"
   value       = aws_ecr_repository.main.name
 }
+
+output "ecr_respository_url" {
+  description = "The URL of the ECR repository"
+  value       = aws_ecr_repository.main.repository_url
+}
